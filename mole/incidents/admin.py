8523368be2ead9b6e2f278ef0d2e7f668737e8b3
Михаил Богачев <bogachevmikhailirkutsk@gmail.com>
2023-02-21 20:17:49 +0800

    Add templates, models, admin models

--- mole/incidents/admin.py
+++ mole/incidents/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import App, Category
+
+class AdminApp(admin.ModelAdmin):
+    """Класс кастомизации модели App"""
+    list_display = ('pk', 'title', 'text', 'pub_date', 'author', 'category',)
+    list_editable = ('category',)
+    search_fields = ('pk', 'title', 'author', 'category',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+    
+
+
+class AdminCategory(admin.ModelAdmin):
+    """Класс кастомизации модели Category"""
+    list_display = ('pk', 'title', 'description',)
+    search_fields = ('title',)
+
+admin.site.register(App, AdminApp)
+admin.site.register(Category, AdminCategory)
--- mole/incidents/models.py
+++ mole/incidents/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Category(models.Model):
+    title = models.CharField(
+        max_length=50,
+        verbose_name='Название категории'
+    )
+    slug = models.SlugField(unique=True)
+    description = models.TextField(
+        max_length=200,
+        verbose_name='Описание категории'
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['pk']
+
+
+class App(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название заявки')
+    text = models.TextField(max_length=500, verbose_name='Текст заявки')
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        verbose_name='Автор заявки'
+    )
+    category = models.ForeignKey(
+        Category,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        #default=Category.objects.get(pk=1)
+    )
+
+    class Meta:
+        verbose_name = 'Заявка'
+        verbose_name_plural = 'Заявки'
+        ordering = ['-pub_date']
